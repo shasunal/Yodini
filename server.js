246e6e8e0a4a0c39c93f565eb725b9e9a7033d81
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2026-03-07 19:05:59 -0500

    shasuna basic layout

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//npx nodemon server.js
+
 //libraries
 
 const express = require("express");
@@ -21,6 +23,20 @@ app.use(express.urlencoded({ extended: true }));
 
 
 //routes
-pp.get("/", (request, response) => {
+app.get("/", (request, response) => {
   response.render("index.njk", { title: "Home" });
+});
+
+app.get("/page2", (request, response) => {
+  response.render("page2.njk", { title: "Page 2" });
+});
+
+app.get("/page3", (request, response) => {
+  response.render("page3.njk", { title: "Page 3" });
+});
+
+
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
 });
