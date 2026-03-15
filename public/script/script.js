61329b8f064897ac71c5e331bf7289fdb59da3cf
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2026-03-15 14:00:20 -0400

    adjusts gsap translation for cards

--- public/script/script.js
+++ public/script/script.js
@@ -1,18 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const deck = document.querySelector('.stack-cards')
+    const deck = document.querySelectorAll('.tarot-card')
     // gsap code here!
 
     let cards = gsap.timeline()
 
-    deck.addEventListener('click', () => {
-        cards.to(deck, {
-            x: '250px',
-            rotateY: -180,
-            start: () => (window.innerHeight * 0.25),
-            end: () => (window.innerHeight * 0.25)
-        })
-        console.log(deck.querySelector('.tarot-card'))
-        const thisCard = deck.querySelector('.tarot-card')
-        thisCard.src= '/images/back-of-card.svg'
-    })
+    deck.forEach((tarot, index) => { // adds flip animation to all tarot cards
+        if(tarot.parentElement != document.querySelector('.stack-cards')){ //ignores stack of cards
+            console.log(index, (tarot.x-500)-100)
+            cards.set(tarot, {
+                x: `-${(tarot.x-500)-100}px`
+
+            })
+            tarot.addEventListener('click', () => { 
+                cards.to(tarot, {
+                    x: `0px`,
+                    rotateY: -180,
+                    start: () => (window.innerHeight * 0.25),
+                    end: () => (window.innerHeight * 0.25)
+                })
+                tarot.src= '/images/back-of-card.svg'
+            })
+        }
+        
+    });
+    // deck.addEventListener('click', () => {
+
+        
+    // })
 });
