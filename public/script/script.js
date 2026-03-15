d501a29aff800dab2d2a288d76e9b6ac9b7057fc
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2026-03-15 19:32:40 -0400

    adds animation for ticket

--- public/script/script.js
+++ public/script/script.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // declare variables
     const stackDeck = document.querySelector('.stack-cards')
     const deck = document.querySelectorAll('.tarot-card')
-    
+    const ticket = document.querySelector('.cont-card')
 
 
     // SHASUNA's TEXT SPEECH
@@ -51,33 +51,13 @@ document.addEventListener("DOMContentLoaded", () => {
     const line3 = document.querySelector(".line-3");
 
 
-    //keep track of click count
-    // let clickCount = 0;
-
-    // if (stackDeck) {
-    //     //event listener for click on stackcard
-    //     stackDeck.addEventListener("click", () => {
-    //     console.log("clicked number:", clickCount);
-
-    //     if (clickCount === 0 && line1) {
-    //         typeWriter(line1, "Your first card describes your current state.");
-    //     }
-
-    //     if (clickCount === 1 && line2) {
-    //         typeWriter(line2,"Your second card describes the actions you must take.");
-    //     }
-
-    //     if (clickCount === 2 && line3) {
-    //         typeWriter(line3,"Your third card describes what the future holds for you.");
-    //     }
-    // //increment click count
-    //     clickCount++;
-    //     });
-    // }
-
-
     // GSAP configuration
     let cards = gsap.timeline()
+    let ticketCont = gsap.timeline()
+
+    ticketCont.set(ticket, {
+        x: '600px',
+    })
 
     deck.forEach((tarot, index) => { // adds flip animation to all tarot cards
         if(tarot.parentElement != document.querySelector('.stack-cards')){ //ignores stack of cards
@@ -86,11 +66,10 @@ document.addEventListener("DOMContentLoaded", () => {
             stackDeck.style.zIndex = '-100' // NOTICE: THIS MADE STACK OF CARDS DISAPPEAR -touch base later
             tarot.style.zIndex = `-${index}0` // move actual tarot cards to top of stack pile
             tarot.style.cursor = 'pointer'
-            console.log(index,tarot.style.zIndex)
-            
+
 
             cards.set(tarot, { // flip animation: ISSUE:: is pretty hardcoded translation & doesn't exactly align with the card-stack 
-                x: `-${(650+(200*(index-1)))}px`
+                x: `-${(500+(200*(index-1)))}px`
                 ,rotation: Math.floor(Math.random() * 10) %2 == 0 ? Math.floor(Math.random() * 7) : -Math.floor(Math.random() * 7)
             })
             tarot.addEventListener('click', () => { 
@@ -105,8 +84,11 @@ document.addEventListener("DOMContentLoaded", () => {
                     typeWriter(line2,"Your second card describes the actions you must take.");
                 }else if(index==2 && line3){
                     typeWriter(line3,"Your third card describes what the future holds for you.");
+                    ticketCont.to(ticket, { // animate continue ticket
+                        delay: .6, 
+                        x: '0px'
+                    })
                 }
-                
             })
         }
     });
