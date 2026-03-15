51f78b1f7f46cffe9b07f9f1ebad6f08d735397b
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2026-03-15 13:12:17 -0400

    removes duplicate script file and adds gsap

--- public/script/script.js
+++ public/script/script.js
@@ -1,1 +1,18 @@
-console.log('test')
+document.addEventListener("DOMContentLoaded", () => {
+    const deck = document.querySelector('.stack-cards')
+    // gsap code here!
+
+    let cards = gsap.timeline()
+
+    deck.addEventListener('click', () => {
+        cards.to(deck, {
+            x: '250px',
+            rotateY: -180,
+            start: () => (window.innerHeight * 0.25),
+            end: () => (window.innerHeight * 0.25)
+        })
+        console.log(deck.querySelector('.tarot-card'))
+        const thisCard = deck.querySelector('.tarot-card')
+        thisCard.src= '/images/back-of-card.svg'
+    })
+});
